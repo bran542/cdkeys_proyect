dc677cb8370e3577646b1b085134522a098d1659
Brandon M <brandonmunoz542@gmail.com>
2022-10-11 14:31:21 -0300

    Database connection

---
+++ configuration/db.php
@@ -0,0 +1,13 @@
+<?php 
+    $server = 'localhost';
+    $username = 'root';
+    $password = '';
+    $database = 'cdkeys_db';
+
+    try {
+        $conn = new PDO("mysql:host=$server;dbname=$database;", $username, $password);
+        //echo 'Connected!';
+    } catch (PDOException $e) {
+        die('Connected failed: '.$e->getMessage());
+    }
+?>
